ee448edd63a033c2ffb2e2be5a2ffd04b8cf3078
Saadatpour, Mojdeh <mojdeh.saadatpour@gft.com>
2026-04-29 14:09:07 +0200

    delet images

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ const App: React.FC = () => {
   const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
 
   useEffect(() => {
-    fetch("/data/cars.json")
+    fetch("/cars.json")
       .then((response) => response.json())
       .then((data) => {
         const carsArray: Car[] = Object.keys(data).map((key) => ({
